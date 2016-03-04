72ef0390f03602560ddf5743af52edb67734f5f4
Andreas Lattner <andreas.lattner@ottogroup.com>
2016-03-04 12:53:39 +0100

    Updated versions of dependencies.

--- palladium/util.py
+++ palladium/util.py
@@ -279,7 +279,7 @@ def memory_usage_psutil():
     """Return the current process memory usage in MB.
     """
     process = psutil.Process(os.getpid())
-    mem = process.get_memory_info()[0] / float(2 ** 20)
+    mem = process.memory_info()[0] / float(2 ** 20)
     return mem
 
 
