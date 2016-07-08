fd6f9bf9cf66892e0bd39d355d97c643053886f8
Daniel Nouri <daniel.nouri@gmail.com>
2016-07-08 15:12:11 -0400

    Attach PluggableDecorator to all DatasetLoader.__call__ methods
    Uses a metaclass so no need to do it individually and per class.

--- palladium/tests/test_interfaces.py
+++ palladium/tests/test_interfaces.py
@@ -22,6 +22,33 @@ class TestAnnotate:
         assert model.__metadata__['one'] == '11'
 
 
+def load_data_decorator(func):
+    def inner(self):
+        X, y = func(self)
+        return self.name + X, self.name + y
+    return inner
+
+
+class TestDatasetLoader:
+    @pytest.fixture
+    def DatasetLoader(self):
+        from palladium.interfaces import DatasetLoader
+        return DatasetLoader
+
+    def test_call_decorator(self, DatasetLoader, config):
+        config['load_data_decorators'] = [
+            'palladium.tests.test_interfaces.load_data_decorator'
+            ]
+
+        class MyDatasetLoader(DatasetLoader):
+            name = 'hey'
+
+            def __call__(self):
+                return 'X', 'y'
+
+        assert MyDatasetLoader()() == ('heyX', 'heyy')
+
+
 class TestPredictError:
     @pytest.fixture
     def PredictError(self):
