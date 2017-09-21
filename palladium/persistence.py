149cbc591eca5ac75331935fc6b82aae8ef23b95
Daniel Nouri <daniel.nouri@gmail.com>
2017-09-21 12:26:40 +0200

    Fix bug with handling of encoding when using RestIO

--- palladium/persistence.py
+++ palladium/persistence.py
@@ -130,11 +130,11 @@ class RestIO(FileLikeIO):
         if mode[0] == 'r':
             res = self.session.get(path, stream=True)
             res.raise_for_status()
-            if res.encoding is not None:
+            if 'b' in mode:
+                return res.raw
+            else:
                 reader = codecs.getreader(res.encoding)
                 return reader(res.raw)
-            else:
-                return res.raw
         elif mode[0] == 'w':
             return self._write(path, mode=mode)
         raise NotImplementedError("filemode: %s" % (mode,))
